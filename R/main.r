f530af60a575608363ee4b7a55f9e7e81b9d9334
Andrew Pound <aepound@gmail.com>
2015-04-09 12:36:29 -0600

    Merged a few changes into the main.r script.

--- R/main.r
+++ R/main.r
@@ -8,6 +8,7 @@ list.of.packages <- c(
                       "kernlab",
                       "nnet",
                       "gbm",
+                      "plyr",
                       "pls",
                       "caret",
                       "matlab"
@@ -16,7 +17,7 @@ list.of.packages <- c(
 check_n_install_packages( list.of.packages )
 
 #====================================================
-fitControl = trainControl(method = "cv", number = 10)
+fitControl = trainControl(method = "cv", number = 3)
 
 #====================================================
 # RF
@@ -24,15 +25,71 @@ fitControl = trainControl(method = "cv", number = 10)
 # Package: randomForest
 # Tuning parameter: mtry
 # Mtry ranges from 1:number of variables
-rfGrid = expand.grid(.mtry = 1:sqrt(length(train)));
-print("Performing Cross-validation...")
-rfFit = train( scale(train.x), as.factor(train.y),"rf", tuneGrid=rfGrid, trControl=fitControl)
-print("Training Forest...")
-fitRF = randomForest( scale(train.x), as.factor(train.y), mtry=as.matrix(rfFit$bestTune))
-print("Running Test set...")
-pred.rf = predict(fitRF, newdata=scale(test.x))
-mse.rf= mean( (pred.rf - test.y)^2 )
-print(rfFit$bestTune)
-print(mse.rf)
+rf.grid = expand.grid(.mtry = 1:sqrt(ncol(train.x)));
+print('Starting 10 factor cross-validation')
+rf.xvaltime = proc.time();
+# Do we really need to scale?  I understand that it's a normal thing done when using randomforest, but is it necessary?
+rf.caretraw = train( train.x,
+                     train.y,"rf",
+                     preProc = c("scale"),
+                     tuneGrid=rf.grid,
+                     trControl=fitControl)
+rf.xvaltime = proc.time() - rf.xvaltime;
+print('Finished cross-validation')
+print('Training RandomForest')
+rf.traintime = proc.time();
+rf.rawforest = randomForest( scale(train.x), train.y, mtry=as.matrix(rf.caretraw$bestTune))
+rf.traintime = proc.time() - rf.traintime;
+trainFitControl = trainControl(method='none',classProbs=FALSE);
+t1 = proc.time();
+rf.rawtrain = train( train.x,
+                     train.y,"rf",
+                     preProc = c("scale"),
+                     tuneGrid=rf.caretraw$bestTune,
+                     trControl=trainFitControl)
+t1 = proc.time() - t1;
+print('Testing RandomForest')
+rf.testtime = proc.time();
+rf.pred = predict(rf.rawtrain, newdata=scale(test.x))
+rf.testtime = proc.time() - rf.testtime;
+print('Done with testing')
+rf.errors = rf.pred != test.y;
+error.rf = sum(errors)/length(rf.errors);
+print('Done calculating error')
+print(rf.caretraw$bestTune)
+print(error.rf)
 
+
+#====================================================
+# Naive Bayes
+#====================================================
+# Package: klaR
+# Tuning parameter: fL, useKernel
+# fL
+# useKernel
+#nb.grid = expand.grid();
+#print('Starting 10 factor cross-validation')
+#nb.xvaltime = proc.time();
+# Do we really need to scale?  I understand that it's a normal thing done when using randomforest, but is it necessary?
+#nb.caretraw = train( train.x,
+#                     train.y,"rf",
+#                     preProc = c("scale"),
+#                     tuneGrid=nb.grid,
+#                     trControl=fitControl)
+#nb.xvaltime = proc.time() - nb.xvaltime;
+#print('Finished cross-validation')
+#print('Training RandomForest')
+#nb.traintime = proc.time();
+#nb.rawforest = randomForest( scale(train.x), train.y, mtry=as.matrix(nb.caretraw$bestTune))
+#nb.traintime = proc.time() - nb.traintime;
+#print('Testing RandomForest')
+#nb.testtime = proc.time();
+#nb.pred = predict(nb.rawforest, newdata=scale(test.x))
+#nb.testtime = proc.time() - nb.testtime;
+#print('Done with testing')
+#nb.errors = nb.pred != test.y;
+#error.nb = sum(errors)/length(nb.errors);
+#print('Done calculating error')
+#print(nb.caretraw$bestTune)
+#print(error.nb)
 
